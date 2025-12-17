a8a46b7609d228d894824b627eee805ef2086a9e
Devika-Chakravarti <devikachakravarti4@gmail.com>
2025-12-17 16:58:49 +0530

    [Devika] : added KilometersToMilesConversion.java

---
+++ gcr-codebase/programming-elements/level-1/KilometersToMilesConversion.java
@@ -0,0 +1,12 @@
+public class KilometersToMilesConversion{
+	public static void main(String args[]){
+
+		double distance = 10.8;
+		
+		// Converting Distance from km to miles
+		double resultantDistance = 10.8 * 1.6;
+
+		// Displaying the calculated conversion 
+		System.out.print("The distance " + distance + " km in miles is " + resultantDistance); 
+	}
+}

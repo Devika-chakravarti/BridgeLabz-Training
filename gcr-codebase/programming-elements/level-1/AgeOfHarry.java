180b459cee405e5e2f5cd4b4be1a6cda8d22e0b1
Devika-Chakravarti <devikachakravarti4@gmail.com>
2025-12-17 16:57:09 +0530

    [Devika] : added AgeOfHarry.java

---
+++ gcr-codebase/programming-elements/level-1/AgeOfHarry.java
@@ -0,0 +1,13 @@
+public class AgeOfHarry{
+		public static void main(String args[]){
+
+			int currentYear = 2024;
+			int birthYear = 2000;
+			
+			// Calculating Harry's present age
+			int presentAge = currentYear-birthYear;
+
+			// Displaying Harry's present age
+			System.out.print("Harry's age in 2024 is " + presentAge);
+	}
+}

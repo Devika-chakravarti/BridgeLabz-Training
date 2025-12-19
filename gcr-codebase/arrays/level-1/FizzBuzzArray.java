671def540b12ba3119a5b10cf2cb9da025dd4313
Devika-Chakravarti <devikachakravarti4@gmail.com>
2025-12-19 20:52:38 +0530

    [Devika] : Added a program to print Fizz, Buzz and FizzBuzz based on the divisibility of 5 and 3 for the user input number

---
+++ gcr-codebase/arrays/level-1/FizzBuzzArray.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class FizzBuzzArray{
+	public static void main(String args[]){
+		
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+	
+		
+		if(number > 0){
+			
+			// Loop is from 0 to number, so array size must be number + 1
+	                String[] result = new String[number + 1];
+
+			for(int i = 0 ; i <= number ; i++){
+				if(i % 3 == 0 && i % 5 == 0){
+					result[i] = "FizzBuzz";
+				}
+				else if(i % 3 == 0){
+					result[i] = "Fizz";
+				}
+				else if(i % 5 == 0){
+					result[i] = "Buzz";
+				}
+				else{
+					result[i] = String.valueOf(i);;
+				}
+			}
+
+			for(int i = 0 ; i <= number ; i++){
+				System.out.println(result[i]);
+			}
+			
+		}
+	
+	}
+}

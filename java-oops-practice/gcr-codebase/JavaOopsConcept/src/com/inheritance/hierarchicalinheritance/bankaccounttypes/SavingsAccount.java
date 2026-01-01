c85eda0e4754cec7dfc9aa88522e6d4d273332c7
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-01 14:09:56 +0530

    [Devika] : Added a SavingsAccount subclass

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hierarchicalinheritance/bankaccounttypes/SavingsAccount.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchicalinheritance.bankaccounttypes;
+class SavingsAccount extends BankAccount {
+
+    private double interestRate;
+
+    public SavingsAccount(int accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    // overriding method
+    public void displayAccountType() {
+        System.out.println("Account Type: Savings Account");
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+}

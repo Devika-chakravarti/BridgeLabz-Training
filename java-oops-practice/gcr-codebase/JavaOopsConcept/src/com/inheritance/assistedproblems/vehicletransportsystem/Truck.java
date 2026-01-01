333686796af49605b7f54a733a1fc38fbf0f29fc
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-01 13:02:56 +0530

    [Devika] : Added a Truck subclass

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/assistedproblems/vehicletransportsystem/Truck.java
@@ -0,0 +1,16 @@
+package com.inheritance.assistedproblems.vehicletransportsystem;
+class Truck extends Vehicle {
+
+    private double loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    // overriding displayInfo method
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}

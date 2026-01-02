0e8d5817e97e095398b6097300dc7c06c098ca14
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-02 14:03:04 +0530

    [Devika] : Added a Reservable interface

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationpolymorphismandabstraction/librarymanagementsystem/Reservable.java
@@ -0,0 +1,6 @@
+package com.encapsulationpolymorphismandabstraction.librarymanagementsystem;
+
+public interface Reservable {
+    void reserveItem(String borrowerName);
+    boolean checkAvailability();
+}

a45c3e5cf806b0df2ceaca5046516be5e90a9957
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-02 14:12:09 +0530

    [Devika] : Added a Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationpolymorphismandabstraction/vehiclerentalsystem/Vehicle.java
@@ -0,0 +1,51 @@
+package com.encapsulationpolymorphismandabstraction.vehiclerentalsystem;
+
+public abstract class Vehicle {
+
+	// Attributes
+    private String vehicleNumber;
+    private String type;
+    private double rentalRate;
+
+    // Constructor
+    public Vehicle(String vehicleNumber, String type, double rentalRate) {
+        this.vehicleNumber = vehicleNumber;
+        this.type = type;
+        this.rentalRate = rentalRate;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getRentalRate() {
+        return rentalRate;
+    }
+
+    // Encapsulation
+    public void setRentalRate(double rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    // Abstract method
+    public abstract double calculateRentalCost(int days);
+
+    // Default insurance behavior
+    public double calculateInsurance() {
+        return 0;
+    }
+
+    public void getInsuranceDetails() {
+        System.out.println("No insurance applicable.");
+    }
+
+    public void displayDetails() {
+        System.out.println("Vehicle Number: " + vehicleNumber);
+        System.out.println("Type: " + type);
+        System.out.println("Rental Rate (per day): " + rentalRate);
+    }
+}

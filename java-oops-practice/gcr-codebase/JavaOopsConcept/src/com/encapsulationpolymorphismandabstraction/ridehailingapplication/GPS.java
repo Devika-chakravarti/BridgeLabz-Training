6ae0d71f753776a1fa4f5913ff65487cf81287ac
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-02 14:08:14 +0530

    [Devika] : Added a GPS interface

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationpolymorphismandabstraction/ridehailingapplication/GPS.java
@@ -0,0 +1,6 @@
+package com.encapsulationpolymorphismandabstraction.ridehailingapplication;
+
+public interface GPS {
+    String getCurrentLocation();
+    void updateLocation(String newLocation);
+}

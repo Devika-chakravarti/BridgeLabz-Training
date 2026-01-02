c8957c0aa4511209ff45fecebe1487a522370e7e
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-02 14:07:27 +0530

    [Devika] : Added a Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationpolymorphismandabstraction/ridehailingapplication/Vehicle.java
@@ -0,0 +1,39 @@
+package com.encapsulationpolymorphismandabstraction.ridehailingapplication;
+
+public abstract class Vehicle {
+
+	// Attributes
+    private String vehicleId;
+    private String driverName;
+    private double ratePerKm;
+
+    // Constructor
+    public Vehicle(String vehicleId, String driverName, double ratePerKm) {
+        this.vehicleId = vehicleId;
+        this.driverName = driverName;
+        this.ratePerKm = ratePerKm;
+    }
+
+    // Getters
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    protected double getRatePerKm() {
+        return ratePerKm;
+    }
+
+    // Concrete method
+    public void getVehicleDetails() {
+        System.out.println("Vehicle ID: " + vehicleId);
+        System.out.println("Driver Name: " + driverName);
+        System.out.println("Rate per Km: " + ratePerKm);
+    }
+
+    // Abstract method
+    public abstract double calculateFare(double distance);
+}

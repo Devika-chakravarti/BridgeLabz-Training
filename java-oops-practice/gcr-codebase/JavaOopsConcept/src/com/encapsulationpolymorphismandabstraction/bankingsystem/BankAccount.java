6376f42daedab5a92d624b6f8f86d7331e84c68f
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-02 12:19:00 +0530

    [Devika] : Added a BankAccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationpolymorphismandabstraction/bankingsystem/BankAccount.java
@@ -0,0 +1,60 @@
+package com.encapsulationpolymorphismandabstraction.bankingsystem;
+
+public abstract class BankAccount {
+
+	// Attributes
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Getters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Encapsulation
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+
+    // Default loan behavior
+    public void applyForLoan() {
+        System.out.println("Loan facility not available.");
+    }
+
+    public double calculateLoanEligibility() {
+        return 0;
+    }
+
+    public void displayDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + holderName);
+        System.out.println("Balance: " + balance);
+    }
+}

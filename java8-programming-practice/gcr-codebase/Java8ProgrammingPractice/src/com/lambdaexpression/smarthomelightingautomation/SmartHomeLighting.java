01635ed32d65003d6ee88471f24a1f4527103698
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-01-29 23:48:11 +0530

    [Devika] : Added code for Smart Home Lighting Automation

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/lambdaexpression/smarthomelightingautomation/SmartHomeLighting.java
@@ -0,0 +1,30 @@
+/*Smart Home Lighting Automation
+○ Scenario: A building has smart lights. Different triggers (motion, time of day, voice
+commands) need to execute different light patterns.
+○ Task: Use lambdas to define these light activation behaviors dynamically without
+creating multiple classes.*/
+
+package com.lambdaexpression.smarthomelightingautomation;
+
+public class SmartHomeLighting {
+	@FunctionalInterface
+	interface LightPattern {
+		void activate();
+	}
+
+	public static void main(String args[]) {
+		// defining light behaviours 
+		LightPattern motionTrigger = () -> System.out.println("Lights ON at full brightness");
+		LightPattern timeOfDayTrigger = () -> System.out.println("Lights set to warm dim mode at evening time");
+		LightPattern voiceCommandTrigger = () -> System.out.println("Lights turned OFF");
+	
+		runLightPattern(motionTrigger);
+		runLightPattern(timeOfDayTrigger);
+		runLightPattern(voiceCommandTrigger);
+	}
+
+	public static void runLightPattern(LightPattern pattern) {
+		// execute selected light behaviour
+		pattern.activate();
+	}
+}
